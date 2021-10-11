572bfee46a13d0e9adff5aa26c064769233e5882
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2021-10-11 01:42:18 +0100

    Added user choice of genre
    Added dialogue to let the user choose the genre of the books to be scraped
    Added a link section to the information of each book, which if followed sends the user to the books page on the website

--- Book_Scraper.py
+++ Book_Scraper.py
@@ -1,6 +1,59 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
 import re
+from urllib.request import urlopen
+
+import pyinputplus as pyip
+from bs4 import BeautifulSoup
+
+categories_list = [
+    "Travel",
+    "Mystery",
+    "Historical Fiction",
+    "Sequential Art",
+    "Classics",
+    "Philosophy",
+    "Romance",
+    "Womens Fiction",
+    "Fiction",
+    "Childrens",
+    "Religion",
+    "Nonfiction",
+    "Music",
+    "Science Fiction",
+    "Sports and Games",
+    "Fantasy",
+    "New Adult",
+    "Young Adult",
+    "Science",
+    "Poetry",
+    "Paranormal",
+    "Art",
+    "Psychology",
+    "Autobiography",
+    "Parenting",
+    "Adult Fiction",
+    "Humor",
+    "Horror",
+    "History",
+    "Food and Drink",
+    "Christian Fiction",
+    "Business",
+    "Biography",
+    "Thriller",
+    "Contemporary",
+    "Spirituality",
+    "Academic",
+    "Self Help",
+    "Historical",
+    "Christian",
+    "Suspense",
+    "Short Stories",
+    "Novels",
+    "Health",
+    "Politics",
+    "Cultural",
+    "Erotica",
+    "Crime",
+]
 
 base_url = "http://books.toscrape.com/"
 
@@ -8,37 +61,56 @@ base_html = urlopen(base_url).read().decode("utf-8")
 
 base_soup = BeautifulSoup(base_html, "html.parser")
 
-categories = base_soup.select("a[href^='catalogue/category/books/']")
+user_choice = pyip.inputMenu(
+    choices=categories_list,
+    prompt="Please choose the genre you'd like to know more about. \n",
+    numbered=True,
+).lower()
 
-travel_url = base_url + categories[0]["href"]
+user_choice = "-".join(user_choice.split())
 
-travel_html = urlopen(travel_url).read().decode("utf-8")
+print(user_choice)
 
-travel_soup = BeautifulSoup(travel_html, "html.parser")
+categories_links = base_soup.select(
+    "a[href^='catalogue/category/books/" + user_choice + "']"
+)
 
-travel_books = travel_soup.select("article")
+genre_url = base_url + categories_links[0]["href"]
 
-print("Travel Books")
+genre_html = urlopen(genre_url).read().decode("utf-8")
 
-for i in range(len(travel_books)):
+genre_soup = BeautifulSoup(genre_html, "html.parser")
+
+genre_books = genre_soup.select("article")
+
+for i in range(len(genre_books)):
 
     title = re.search(
-        'title="(?P<title>.*?)"', str(travel_books[i].select("a[title]")[0])
+        'title="(?P<title>.*?)"', str(genre_books[i].select("a[title]")[0])
     ).group("title")
 
     rating = re.search(
         'class="star-rating (?P<rating>.*?)"',
-        str(travel_books[i].select("p[class^='star']")[0]),
+        str(genre_books[i].select("p[class^='star']")[0]),
     ).group("rating")
 
-    price = str(travel_books[i].select('p[class="price_color"]')[0])[23:29]
+    price = str(genre_books[i].select('p[class="price_color"]')[0])[23:29]
+
+    link = re.search(
+        'href="../../../(?P<link>.*?)/index.html"',
+        str(genre_books[i].find_all("h3")),
+    ).group("link")
+
+    link = base_url + "catalogue/" + link
 
     print(
-        f"""Book {i + 1} of {len(travel_books)}
+        f"""Book {i + 1} of {len(genre_books)}
         
         Title: {title}
         
         Rating: {rating} stars
         
-        Price: {price}"""
+        Price: {price}
+        
+        Link: {link}"""
     )
